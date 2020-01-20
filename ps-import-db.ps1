27c285feff97768c82ec1c8b8ce02a2f5334ac33
tr0ubl3 <mentor.1985@gmail.com>
2020-01-20 22:27:57 +0200

    implementare conexiune la baza de date locala

---
+++ ps-import-db.ps1
@@ -0,0 +1,11 @@
+﻿# import sqlite dll from absolute path
+add-type -Path "D:\working\delphi\ps-import-db\sqlite\x64\1.0.112.0\System.Data.SQLite.dll"
+
+# adaugare cale bd
+$con_str = "data source=D:\working\delphi\capabilitati\sqlite for excel\DataCapa.db3"
+
+$con_obj = New-Object -TypeName System.Data.SQLite.SQLiteConnection
+
+$con_obj.ConnectionString = $con_str
+
+$con_obj.Open()
