ff6ba0babf8cf54991be8de8b38aeaaec65d2fb4
tr0ubl3 <mentor.1985@gmail.com>
2020-02-29 19:42:53 +0200

    verificare validitate fisier text

--- ps-import-db.ps1
+++ ps-import-db.ps1
@@ -1,5 +1,5 @@
 ﻿# import sqlite dll from absolute path
-add-type -Path "D:\working\delphi\ps-import-db\sqlite\x64\1.0.112.0\System.Data.SQLite.dll"
+add-type -Path "sqlite\x64\1.0.112.0\System.Data.SQLite.dll"
 
 # cale bd
 $con_str = "data source=D:\working\delphi\capabilitati\sqlite for excel\DataCapa.db3"
@@ -8,12 +8,14 @@ $con_obj = New-Object -TypeName System.Data.SQLite.SQLiteConnection
 
 $con_obj.ConnectionString = $con_str
 $con_obj.Open()
+
 $n=0
 foreach($line in Get-Content D:\working\delphi\ps-import-db\misc\op180\1.txt) {
-    $n++
-    if($n -eq 1){
-        $line.split(tab)
-        write-host $line
+    # write-host $n
+    if($n -eq 0){
+        # $nnn = $line.replace("`t",",")
+        $nnn = $line.split("`t")
+        write-host $nnn[0]
     }
-    #write-host $n
+    $n++
 }
