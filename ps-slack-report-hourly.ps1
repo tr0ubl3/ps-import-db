4e8aacb06d83de46f16cad115a6a166d69b58201
tr0ubl3 <mentor.1985@gmail.com>
2020-03-15 22:28:27 +0200

    implementare raportare slack

---
+++ ps-slack-report-hourly.ps1
@@ -0,0 +1,14 @@
+﻿$uriSlack = "https://hooks.slack.com/services/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"
+
+$body = ConvertTo-Json @{
+    pretext = "Test implementare"
+    text = "Hello from powershell!!!"
+    color = "#FF0000"
+}
+
+
+try {
+    Invoke-RestMethod -uri $uriSlack -Method Post -body $body -ContentType 'application/json' | Out-Null
+} catch {
+    Write-Error (Get-Date) ": Update to Slack went wrong..."
+}
